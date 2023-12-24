2c6ad4bf07b5008d104a7e462d4d07d9079f4ca6
Jamie Bowers <james.t.k.bowers@gmail.com>
2023-12-24 22:18:15 +1300

    Remove default packages from vm image.

--- crosvm.nix
+++ crosvm.nix
@@ -24,6 +24,7 @@
   networking.dhcpcd.enable = false;
   documentation.man.enable = false;
   documentation.nixos.enable = false;
+  environment.defaultPackages = [];
 
   boot.initrd.kernelModules = [
     "squashfs"
